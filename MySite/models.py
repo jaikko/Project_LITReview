0cd594710538a8b9550d3fee0abcfae0929eda88
jaikko <65661383+jaikko@users.noreply.github.com>
2021-03-31 16:45:31 +0200

    end

---
+++ MySite/models.py
@@ -0,0 +1,58 @@
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.db.models.fields import EmailField, DateTimeField
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    is_staff= None
+    is_superuser= None
+    email = None
+    username = models.CharField(max_length=70, blank=False, unique= True)
+    password = models.CharField(max_length=100, blank=False)
+    USERNAME_FIELD = 'username'
+    
+class Ticket(models.Model):
+    title= models.CharField(max_length=128) 
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image =models.ImageField(null=True, blank=True, upload_to = 'images/')
+    time_created = models.DateTimeField(auto_now_add=True)
+    
+    
+
+class UserFollows(models.Model):
+    # Your UserFollows model definition goes here
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followed_by")
+    class Meta:   
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = ('user', 'followed_user', )
+        
+    
+    def get_id_follow(user):
+        liste = []
+        follow = UserFollows.objects.filter(user_id=user.id)
+        for i in follow:
+            liste.append(i.followed_user.id) 
+        return liste
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE, related_name = "reviews")
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+    
---
+++ MySite/templatetags/tag_library.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter()
+def to_int(value):
+    return int(value)
+
+@register.filter(name='range')
+def filter_range(start, end):
+    return range(start, end)
