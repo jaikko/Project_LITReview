97c47c52af1300abf4c66e31375b46b19e9ec87d
jaikko <65661383+jaikko@users.noreply.github.com>
2021-04-13 17:08:07 +0200

    Update tag_library.py

--- MySite/templatetags/tag_library.py
+++ MySite/templatetags/tag_library.py
@@ -1,9 +1,11 @@
 from django import template
 register = template.Library()
 
+
 @register.filter()
 def to_int(value):
     return int(value)
+
 
 @register.filter(name='range')
 def filter_range(start, end):
