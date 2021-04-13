3f737bb02a4410859782bb6d55cc9aed949bb47e
jaikko <65661383+jaikko@users.noreply.github.com>
2021-04-13 17:06:39 +0200

    Update models.py

--- MySite/models.py
+++ MySite/models.py
@@ -1,3 +1,4 @@
+
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models.fields import EmailField, DateTimeField
@@ -11,31 +12,35 @@ from django.conf import settings
 class User(AbstractUser):
     first_name = None
     last_name = None
-    is_staff= None
-    is_superuser= None
+    is_staff = None
+    is_superuser = None
     email = None
-    username = models.CharField(max_length=70, blank=False, unique= True)
+    username = models.CharField(max_length=70, blank=False, unique=True)
     password = models.CharField(max_length=100, blank=False)
     USERNAME_FIELD = 'username'
-    
+
+
 class Ticket(models.Model):
-    title= models.CharField(max_length=128) 
+    title = models.CharField(max_length=128)
     description = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image =models.ImageField(null=True, blank=True, upload_to = 'images/')
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
     time_created = models.DateTimeField(auto_now_add=True)
-    
+
+
 class UserFollows(models.Model):
     # Your UserFollows model definition goes here
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
     followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followed_by")
-    class Meta:   
+
+    class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
-        unique_together = ('user', 'followed_user', )
-        
+        unique_together = ('user', 'followed_user')
+
+
 class Review(models.Model):
-    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE, related_name = "reviews")
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE, related_name="reviews")
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)])
